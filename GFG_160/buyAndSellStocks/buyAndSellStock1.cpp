b2818a54d813174ac4f90e13af42ae98a1ed6778
Atul Mangla <155222024+atul21mangla@users.noreply.github.com>
2024-12-30 18:16:23 +0530

    Update buyAndSellStock1.cpp

--- GFG_160/buyAndSellStocks/buyAndSellStock1.cpp
+++ GFG_160/buyAndSellStocks/buyAndSellStock1.cpp
@@ -1,17 +1,47 @@
 // Problem link - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+// problem link - https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/buy-stock-2
+
+// TC - O(N)
+// SC - O(1)
 
 class Solution {
-public:
-    int maxProfit(vector<int>& prices) {
-        int maxProfit = 0;
-        int currMin = INT_MAX;
-        for(int x:prices){
-            currMin = min(currMin,x);
-            maxProfit = max(maxProfit,x-currMin);
+  public:
+    int maximumProfit(vector<int> &prices) {
+        int n = prices.size();
+        int minSoFar = 1e9;
+        int profit = 0;
+        for(int i=0;i<n;i++){
+            if(prices[i]<minSoFar){
+                minSoFar = prices[i];
+            }
+            profit = max(profit,prices[i]-minSoFar);
         }
-        return maxProfit;
+        return profit;
     }
 };
+
+
+// brute force
+// TC - O(N*2)
+// SC - O(1)
+
+class Solution {
+  public:
+    int maximumProfit(vector<int> &prices) {
+        int n = prices.size();
+        int res = 0;
+  
+        // Explore all possible ways to buy and sell stock
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                res = max(res, prices[j] - prices[i]);
+            }
+        }
+        return res;
+    }
+};
+
+
 
 // You are given an array prices where prices[i] is the price of a given stock on the ith day.
 
